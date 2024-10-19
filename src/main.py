f242b3f277eca88aeceda651ac1f8dde15b30eb0
Angel Ramirez <angelra@gmail.com>
2024-10-19 05:55:40 -0700

    Cell class wip

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from window import *
 def main():
     win = Window(800, 600)
     
-    line = Line(Point(50, 50), Point(50, 550))
-    win.draw_line(line, "white")
-    line = Line(Point(50, 50), Point(750, 50))
-    win.draw_line(line, "white")
-    line = Line(Point(750, 50), Point(750, 550))
-    win.draw_line(line, "white")
-    line = Line(Point(750, 550), Point(50, 550))
-    win.draw_line(line, "white")
+    # line = Line(Point(50, 50), Point(50, 550))
+    # win.draw_line(line, "white")
+    # line = Line(Point(50, 50), Point(750, 50))
+    # win.draw_line(line, "white")
+    # line = Line(Point(750, 50), Point(750, 550))
+    # win.draw_line(line, "white")
+    # line = Line(Point(750, 550), Point(50, 550))
+    # win.draw_line(line, "white")
     
     win.wait_for_close()
     
--- src/window.py
+++ src/window.py
@@ -37,3 +37,35 @@ class Line():
         
     def draw(self, canvas, fill_color="white"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color)
+        
+class Cell():
+    def __init__(self, win,
+        has_left_wall = True,
+        has_right_wall = True,
+        has_top_wall = True,
+        has_bottom_wall = True,
+    ):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = 0
+        self._x2 = 0
+        self._y1 = 0
+        self._y2 = 0
+        self._win = win
+
+    def draw(self, p1, p2):
+        self._x1 = p1.x
+        self._y1 = p1.y
+        self._x2 = p2.x
+        self._y2 = p2.y
+        
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self.y2)))
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self.y2)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self.y2)))
